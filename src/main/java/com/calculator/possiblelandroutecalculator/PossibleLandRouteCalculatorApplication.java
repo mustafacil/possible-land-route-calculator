55217e8776df02ffed81bb16a3a6f0d2bdc2fa3b
mustafacil <mcilmustafacil@gmail.com>
2021-12-30 21:26:34 +0300

    Project created

---
+++ src/main/java/com/calculator/possiblelandroutecalculator/PossibleLandRouteCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.possiblelandroutecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PossibleLandRouteCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PossibleLandRouteCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calculator/possiblelandroutecalculator/PossibleLandRouteCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.possiblelandroutecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PossibleLandRouteCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
